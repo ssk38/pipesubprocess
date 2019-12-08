dc8e15650ed9a9fb2368a9b553349660fe59b0a0
三輪周作 <shusakumiwa@gmail.com>
2019-12-09 02:00:09 +0900

    classifieres moved from setup.cfg to setup.py because of error

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ def main():
     setup(
         setup_require=['setuptools_scm'],
         use_scm_version=True,
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "License :: OSI Approved :: MIT License"],
     )
 
 if __name__ == "__main__":
